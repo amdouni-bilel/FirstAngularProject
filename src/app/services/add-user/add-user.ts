14ebc4cc7fae510aeb8d2a299dd8132d7448b249
amdouni-bilel <amdounibilel1@gmail.com>
2025-07-07 10:36:03 +0100

    add user

--- src/app/services/add-user/add-user.ts
+++ src/app/services/add-user/add-user.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {NgIf} from '@angular/common';
+import {UsersModel} from '../users.model';
+import {UsersService} from '../users-service';
 
 @Component({
   selector: 'app-add-user',
@@ -13,19 +15,29 @@ import {NgIf} from '@angular/common';
 })
 export class AddUser {
   userForm : FormGroup ;
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder , private userService: UsersService) {
     this.userForm = this.fb.group({
-      name : ['' , Validators.required],
-      email : ['' , Validators.required , Validators.email],
-      age : ['' , Validators.required , Validators.min(18)],
-    })
-  }
-  submitForm(){
-    if(this.userForm.valid){
-      console.log('Formulaire soumis :', this.userForm.value);
-      alert("Formulaire valide avec un message");
-    }
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      age: ['', [Validators.required, Validators.min(18), Validators.max(60)]]
+    });
 
+  }
+  submitForm(): void {
+    if (this.userForm.valid) {
+      const user: Partial<UsersModel> = this.userForm.value;
+      this.userService.addUser(user).subscribe({
+        next: (res) => {
+          console.log('Utilisateur ajouté :', res);
+          alert(`Utilisateur ${res.name} ajouté avec succès !`);
+          this.userForm.reset();
+        },
+        error: (err) => {
+          console.error('Erreur lors de l’ajout', err);
+          alert('Une erreur est survenue');
+        }
+      });
+    }
   }
 
 }
--- src/app/services/users-service.ts
+++ src/app/services/users-service.ts
@@ -1,15 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {UsersModel} from './users.model';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
   private apiUrl = 'http://localhost:8080/api/users';
-  constructor(private http : HttpClient) {}
 
-  getUsers(){
+  constructor(private http: HttpClient) {
+  }
+
+  getUsers() {
     return this.http.get<UsersModel[]>(this.apiUrl)
   }
+
+  addUser(user: Partial<UsersModel>): Observable<UsersModel> {
+    return this.http.post<UsersModel>(this.apiUrl, user)
+  }
+
+
 }
